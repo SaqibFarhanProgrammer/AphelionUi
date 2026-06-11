6c42b351cfb954d04b3922624e06cc0bddc3bed9
SaqibFarhanProgrammer <saqibkhatai@gmail.com>
2026-06-11 16:51:05 +0500

    a

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+};
+
+export default config;
